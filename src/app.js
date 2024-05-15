e9ae1b999603cb08aba16e91475281bff76f7770
Jaume153 <163338482+Jaume153@users.noreply.github.com>
2024-05-15 11:27:27 +0000

    el nuevo generador de excusas

--- src/app.js
+++ src/app.js
@@ -7,5 +7,35 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  const who = [
+    `Un unicornio`,
+    `El Yeti`,
+    `Un extraterrestre`,
+    `Mi doble del futuro`
+  ];
+  const action = [`robó`, `secuestró`, `teletransportó`, `escondió`];
+  const what = [
+    `la luna llena`,
+    `el plato principal`,
+    `la fórmula secreta`,
+    `el último helado del universo`
+  ];
+  const when = [
+    `durante un eclipse lunar`,
+    `mientras viajaba en el tiempo`,
+    `cuando nadie miraba`,
+    `en una dimensión paralela`
+  ];
+
+  function excusaRandom() {
+    const randomWho = who[Math.floor(Math.random() * who.length)];
+    const randomAction = action[Math.floor(Math.random() * action.length)];
+    const randomWhat = what[Math.floor(Math.random() * what.length)];
+    const randomWhen = when[Math.floor(Math.random() * when.length)];
+
+    return `${randomWho} ${randomAction} ${randomWhat} ${randomWhen}.`;
+  }
+
+  document.querySelector("#excusa").innerHTML = excusaRandom();
+  console.log(excusaRandom());
 };
